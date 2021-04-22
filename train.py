e8f18e30c52783de1655abdb408010fff39cc8f0
Hassoubah <mohassoubah@gmail.com>
2021-04-22 13:13:31 +0200

    cherry pick commit 1e0c15107891ff206584122d1b78e18bc72f20bf

--- train.py
+++ train.py
@@ -66,8 +66,9 @@ if __name__ == "__main__":
     args.root_path = dataset_config[dataset_name]['root_path']
     args.list_dir = dataset_config[dataset_name]['list_dir']
     args.is_pretrain = True
-    args.exp = 'TU_' + dataset_name + str(args.img_size)
-    snapshot_path = "../model/{}/{}".format(args.exp, 'TU')
+    args.exp = 'TU_' + dataset_name + str(args.img_size)#TU_Synapse224
+    #creats a path with a name that has all the properties of the experiment
+    snapshot_path = "../model/{}/{}".format(args.exp, 'TU')#../model/TU_Synapse224/TU
     snapshot_path = snapshot_path + '_pretrain' if args.is_pretrain else snapshot_path
     snapshot_path += '_' + args.vit_name
     snapshot_path = snapshot_path + '_skip' + str(args.n_skip)
